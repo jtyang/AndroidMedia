e9f4a6d41025fb7a5f454febdc0a127061165b94
yangjiantong <yangjiantong1988@126.com>
2017-09-17 23:10:15 +0800

    add Telegram libtgvoip source

---
+++ medialib/src/main/java/com/yjt/androidmedia/lib/utils/XLog.java
@@ -0,0 +1,33 @@
+package com.yjt.androidmedia.lib.utils;
+
+/**
+ * xlog
+ * DATE: 2017/9/17
+ */
+public class XLog {
+
+    public static void d(String msg) {
+
+    }
+
+    public static void i(String msg) {
+
+    }
+
+    public static void w(String msg) {
+
+    }
+
+
+    public static void e(String msg) {
+
+    }
+
+    public static void e(String msg, Throwable e) {
+
+    }
+
+    public static void e(Throwable e) {
+
+    }
+}
